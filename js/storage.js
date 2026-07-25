209b2ff348242e6e9a67c3743721e7a0a098a50a
Daniel Kleynhans <dan16arm@gmail.com>
2026-07-25 10:59:07 +0200

    created  storage.js

---
+++ js/storage.js
@@ -0,0 +1,23 @@
+// storage.js
+
+export function saveData(key, data) {
+    localStorage.setItem(key, JSON.stringify(data));
+}
+
+export function loadData(key) {
+    const storedData = localStorage.getItem(key);
+
+    if (!storedData) {
+        return null;
+    }
+
+    return JSON.parse(storedData);
+}
+
+export function removeData(key) {
+    localStorage.removeItem(key);
+}
+
+export function clearStorage() {
+    localStorage.clear();
+}
